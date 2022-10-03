274bd0f299cd7e5ed5052b76ba8419028ae0f313
Muhammad <79977980+muhhharmmard@users.noreply.github.com>
2022-10-03 13:23:01 +0100

    Update config.ts

--- client/src/config.ts
+++ client/src/config.ts
@@ -7,5 +7,5 @@ export const authConfig = {
   // domain: 'dev-nd9990-p4.us.auth0.com',
   domain: 'dev-5v8eb7o9.us.auth0.com',            // Auth0 domain
   clientId: 'xsIj1oLU6BfLWxlec92wMADjzSV1egEC',          // Auth0 client id
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://localhost:3000/callbacks/'
 }
