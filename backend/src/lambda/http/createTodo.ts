023c30319328110546d3f40d2c6eea33934d752a
Dmay2000 <muwwal4@gmail.com>
2022-10-06 13:44:36 -0400

    more dits

--- backend/src/lambda/http/createTodo.ts
+++ backend/src/lambda/http/createTodo.ts
@@ -1,3 +1,4 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 import * as middy from "middy";
 import { cors, httpErrorHandler } from "middy/middlewares";
 import { CreateTodoRequest } from "../../requests/CreateTodoRequest";
